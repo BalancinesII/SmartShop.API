0c42071bda89bf76eece31e797fe63c6ba522304
BalancinesII <ignaciosaizahumada@hotmail.com>
2026-09-13 13:07:26 +0200

    Add EF Core migration for Product.ImageUrl

--- SmartShop.Infrastructure/Migrations/20260913104929_AddProductImageUrl.Designer.cs
+++ SmartShop.Infrastructure/Migrations/20260913104929_AddProductImageUrl.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmartShop.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using SmartShop.Infrastructure.Persistence;
 namespace SmartShop.Infrastructure.Migrations
 {
     [DbContext(typeof(SmartShopDbContext))]
-    partial class SmartShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260913104929_AddProductImageUrl")]
+    partial class AddProductImageUrl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
